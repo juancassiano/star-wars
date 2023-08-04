f4bcf3afe91fa751f950f94c5ddd55d790fdea47
juancassiano <juancassiano@hotmail.com>
2023-08-04 13:41:54 -0300

    Teste de integração: Salvando no banco

---
+++ star-wars-planet-api/src/test/java/com/example/starwarsplanetapi/domain/respository/PlanetRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.starwarsplanetapi.domain.respository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import com.example.starwarsplanetapi.domain.model.Planet;
+
+import static com.example.starwarsplanetapi.domain.common.PlanetConstants.PLANET;
+
+@DataJpaTest
+public class PlanetRepositoryTest {
+  @Autowired
+  private PlanetRepository planetRepository;
+
+  @Autowired
+  private TestEntityManager testEntityManager;
+
+  @Test
+  public void createPlanet_WithValidData_ReturnsPlanet(){
+    Planet planet = planetRepository.save(PLANET);
+
+    Planet sut = testEntityManager.find(Planet.class, planet.getId());
+    
+    assertThat(sut).isNotNull();
+    assertThat(sut.getName()).isEqualTo(PLANET.getName());
+    assertThat(sut.getClimate()).isEqualTo(PLANET.getClimate());
+    assertThat(sut.getTerrain()).isEqualTo(PLANET.getTerrain());
+
+  }
+  
+}
