67b670d6b822049a8e4b34e473f8613afe77b4b6
juancassiano <juancassiano@hotmail.com>
2023-08-08 15:09:39 -0300

    Configuração debanco de dados  para teste

--- star-wars-planet-api/src/test/java/com/example/starwarsplanetapi/it/PlanetsIT.java
+++ star-wars-planet-api/src/test/java/com/example/starwarsplanetapi/it/PlanetsIT.java
@@ -2,8 +2,10 @@ package com.example.starwarsplanetapi.it;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetsIT {
 
